e449dea1571384d4391aafb9367aa47880341848
Jon Ellwood <jon.ellwood@berkeleycountysc.gov>
2024-01-02 08:30:32 -0500

    init commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,103 @@
+<script>
+	import logo from '$lib/images/BCLogo.png';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://berkeleycountysc.gov/"> <img src={logo} alt="bc-logo" /></a>
+	</div>
+	<nav class="container">
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/theme">Theme</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		view-transition-name: header;
+	}
+
+	.corner {
+		width: 20em;
+		height: 3em;
+		margin-top: 15px;
+		margin-left: 7%;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 20em;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+		border-bottom-left-radius: 1px;
+		border-top-right-radius: 1px;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+		view-transition-name: active-page;
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
