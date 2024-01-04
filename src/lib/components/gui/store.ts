9d2b9a96928c07a1cef1737e0a678fa8ab6830fb
Jon Ellwood <jonellwood@gmail.com>
2024-01-04 15:53:08 -0500

    more to commit

--- src/lib/components/gui/store.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { writable } from 'svelte/store';
-
-export function guiControls(values: any) {
-	return writable(values);
-}
--- src/routes/api/getCountyStoreData/getEmpData.js
+++
@@ -1,9 +0,0 @@
-async function getEmpData(emp) {
-	await fetch('https://store.berkeleycountysc.gov/getEmpData.php?empNum=' + emp)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		});
-}
-
-export default getEmpData;
